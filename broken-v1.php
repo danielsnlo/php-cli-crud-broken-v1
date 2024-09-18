996049d22170feeec8dfe5ad90c307cc45c7d31b
zirodev <zirodev23@zirodev23.com>
2024-09-18 08:42:00 +0300

    library broken v1 added

---
+++ broken-v1.php
@@ -0,0 +1,79 @@
+<?php
+
+$books = [
+    1 => [
+        'title' => 'The Great Gatsby',
+        'author' => 'F. Scott Fitzgerald'
+    ],
+    2 => [
+        'title' => '1984',
+        'author' => 'George Orwell'
+    ],
+    3 => [
+        'title' => 'Pride and Prejudice',
+        'author' => 'Jane Austen'
+    ]
+];
+
+
+function showAllBooks($books) {
+    foreach ($books as $id => $book) {
+        // need to display each book here
+    }
+}
+
+function showBook() {
+    $id = readline("Enter book id: ");
+    displayBook($id, $books[$id]);
+}
+
+function addBook(&$books) {
+    $title = readline("Enter title: ");
+    $books[] = ['title' => $title, 'author' => $author];
+}
+
+function deleteBook(&$books) {
+    $id = readline("Enter book ID you want to delete: ");
+}
+
+function displayBook($id, $book) {
+    echo "ID: {$id} // Title: ". $book['title'] . " // Author: " . $book['author']. "\n\n";
+}
+
+
+echo "\n\nWelcome to the Library\n";
+$continue = true; 
+do {
+    echo "\n\n";
+    echo "1 - show all books\n";
+    echo "2 - show a book\n";
+    echo "3 - add a book\n";
+    echo "4 - delete a book\n";
+    echo "5 - quit\n\n";
+    $choice = readline();
+
+    switch ($choice) {
+        case 1:
+            showAllBooks($books);
+            break;
+        case 2:
+            showBook($books);
+            break;
+        case 3:
+            addBook($books);
+            break;
+        case 4:
+            deleteBook($books);
+            break;
+        case 5:
+            echo "Goodbye!\n";
+            $continue = false;
+            break;
+        case 13:
+            print_r($books); // hidden option to see full $books content
+            break;
+        default:
+            echo "Invalid choice\n";
+    };
+
+} while ($continue == true);
